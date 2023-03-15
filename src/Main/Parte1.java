8104c57efa3d70d087cadd24e228581698f6277e
JuanAndres Jaramillo <ja.jaramillop@uniandes.edu.co>
2023-03-15 12:44:12 -0500

    floyd-warshall

--- src/Main/Parte1.java
+++ src/Main/Parte1.java
@@ -13,31 +13,48 @@ public class Parte1 {
         System.out.println("Ingrese el algoritmo que desea usar");
         Scanner input= new Scanner(System.in);
         System.out.println("1. DIJSKTRA");
+        System.out.println("2. FLOYD-WARSHALL");
+        System.out.println("3. BELLMAN-FORD");
+        int i= Integer.parseInt(input.nextLine());
 
-        String line= input.nextLine();
-        int i= Integer.parseInt(line);
+        System.out.println("Ingrese la ruta del archivo txt que contiene la matriz");
+        String ruta= input.nextLine();
 
+        int[][] matriz= readInput(ruta);
+        int [][] res = new int[matriz.length][matriz.length];
+        long tInicio = -1;
+        long tFin = 0;
         switch(i){
             case 1:
-            // input= new Scanner(System.in);
-            System.out.println("Ingrese la ruta del archivo txt que contiene la matriz");
-            String ruta= input.nextLine();
-            int[][] m= readInput(ruta);
-            int [][] res = dijsktraGeneral(m);
-            printMatriz(res);
+                tInicio = System.currentTimeMillis();
+                res = dijsktraGeneral(matriz);
+                tFin = System.currentTimeMillis();
             break;
+            case 2:
+                tInicio = System.currentTimeMillis();
+                res = floydWarschall(matriz);
+                tFin = System.currentTimeMillis();
+            break;
+            case 3:
+            break;
+            default:
+                System.out.println("Debe ingresar un numero entre 1 y 3.");
         }
-
+        printMatriz(res);
+        System.out.println("\n--------------------------------------\ntiempo de ejecucion del algoritmo de DIJSKTRA: " 
+                            + (tFin-tInicio) + " milisegundos\n--------------------------------------\n");
         input.close();
     }
 
-    //funcion para imprimir una matriz cuadrada
+    //-------------------------------------------------------------------------
+    // FUNCION DE IMPRESION DE LA MATRIZ 
+    //-------------------------------------------------------------------------
     public static void printMatriz(int[][]m){
         for(int i=0;i<m[0].length;i++){
-            System.out.println("Fuente:" +i); 
             for (int j=0;j<m[0].length;j++){
-                System.out.println(m[i][j]);
+                System.out.print(m[i][j]+"\t");
             }
+            System.out.println();
             
         }
     }
@@ -50,6 +67,7 @@ public class Parte1 {
             
             BufferedReader br= new BufferedReader(new FileReader(ruta));
             String linea = br.readLine().trim();
+
             String [] f=linea.split("\t");
 			int longitud = f.length;
 			m = new int[longitud][longitud];
@@ -57,7 +75,13 @@ public class Parte1 {
             while (linea!=null){
                 f=linea.split("\t");
                 for (int i=0; i<f.length;i++){
-                    m[fila][i]=Integer.parseInt(f[i]);
+
+                    if(Integer.parseInt(f[i])==-1){
+                        m[fila][i]= Integer.MAX_VALUE;
+                    }
+                    else{
+                        m[fila][i]=Integer.parseInt(f[i]);
+                    }
                 }
                 fila++;
                 linea=br.readLine();
@@ -67,6 +91,7 @@ public class Parte1 {
             System.out.println("No se encuenta el archivo");
             e.printStackTrace();
         }
+
         return m;
     }
 
@@ -130,5 +155,34 @@ public class Parte1 {
         return false;
     }
 
+
+    //-------------------------------------------------------------------------
+    // ALGORITMO DE Floyd-Warschall
+    //-------------------------------------------------------------------------
+
+    public static int[][] floydWarschall(int[][] matrix){
+
+        for(int k = 0; k < matrix.length; k++){
+
+            for (int i = 0; i < matrix.length; i++) {
+
+                for (int j = 0; j < matrix.length; j++) {
+                    
+                    try{
+                        if(Math.addExact(matrix[i][k],  matrix[k][j])< matrix[i][j]){
+                            matrix[i][j] = matrix[i][k] + matrix[k][j];
+                        }
+
+                    }catch(ArithmeticException e){
+                    }
+
+                }
+            }
+        }
+
+        return matrix;
+
+    }
+   
 
 }
